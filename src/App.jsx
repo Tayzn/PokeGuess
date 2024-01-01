4c7b804a0273c864e2e343774ba5b69576fad794
Tayzn <tyguy2464@gmail.com>
2024-01-01 17:00:51 -0500

    Added Pokemon Font

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { Container, Col } from 'react-bootstrap'
 import { Header } from './components/Header'
 import { Home } from './components/Home'
 
+import './App.css'
+
 function App() {
   return (
-    <Container fluid className='m-0 p-0 vh-100'>
+    <Container
+      fluid
+      className='m-0 p-0 vh-100'
+    >
       <Col className='h-100 d-flex flex-column'>
         <Header />
         <Home />
